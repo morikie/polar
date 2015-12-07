6dceecaca8cc10f07c69a8c77772bf9212550f15
Moritz K <moritz.k@fu-berlin.de>
2015-12-07 16:20:29 +0100

    minor update to sensitivity test

--- perf_testing/perf_fuzzy.cpp
+++ perf_testing/perf_fuzzy.cpp
@@ -29,8 +29,9 @@ int main (int argc, char * argv[]) {
 	fs::path refGenomeIndex = "reference_genome/hg19/reference_genome.fa.fai";
 	std::vector<KnownPolyA> knownPolyAvec;
 	RefGeneParser refGene(refGeneFile);	
-	size_t correctMatches = 0;
-	size_t incorrectMatches = 0;
+	
+	size_t correctMatches = 0; //found true positives
+	size_t incorrectMatches = 0; //not found true positives (false negatives)
 	size_t unknMotives = 0;
 	size_t analyzedSequences = 0;
 
@@ -44,14 +45,18 @@ int main (int argc, char * argv[]) {
 		std::string baseSeqId;
 		qi::parse(knownPolyA.id.begin(), knownPolyA.id.end(), *~qi::char_('.'), baseSeqId);
 		RefGeneProperties refGeneProp = refGene.getValueByKey(baseSeqId);
+		//ignoring empty entries
 		if (knownPolyA.seq.empty() || refGeneProp.chr.empty()) continue;
+		//ignoring entries from not processable chromosome descriptions (e.g. chr6_hash_map... etc.)
 		if (refGeneProp.chr.size() > 6) continue;
 		
+		//calculate size of the transcript by using exon start/end values (using sequence length doesn't work)
 		size_t txLength = 0;
 		for (size_t i = 0; i < refGeneProp.exonStarts.size(); i++) {
 			txLength += refGeneProp.exonEnds[i] - refGeneProp.exonStarts[i];
 		}
-
+		
+		//mapping transcript position of the PAS to the genomic position (each position is a true positive)
 		BOOST_FOREACH (size_t & pos, knownPolyA.polyApos) {
 			size_t geneticPos;
 			if (refGeneProp.strand == "+") {
@@ -62,12 +67,14 @@ int main (int argc, char * argv[]) {
 			truePositives[refGeneProp.chr].push_back(std::make_pair(geneticPos, refGeneProp.strand));
 		}
 	}
-	
+	//evaluating every true positive
 	for (auto it = truePositives.begin(); it != truePositives.end(); it++) {
+		//iterating over every true PAS
 		for (auto vecIt = it->second.begin(); vecIt != it->second.end(); vecIt++) {
 			size_t & pos = vecIt->first;
 			std::string & strand = vecIt->second;
 			seqan::CharString temp;
+			//mapping chromosome to id number for use in the fasta index
 			unsigned idx = UINT_MAX;
 			if (it->first == "chrX") {
 				idx = 22;
@@ -80,7 +87,7 @@ int main (int argc, char * argv[]) {
 				//adjusting idx to 0-starting map
 				idx--;
 			}
-			
+			//copy the genomic sequence 100 bases around the genomic pos of the PAS (200nt long)
 			seqan::readRegion(temp, faiIndex, idx, pos - 100, pos + 100);
 			SeqStruct ss = {
 				std::string(seqan::toCString(temp)),
@@ -92,12 +99,10 @@ int main (int argc, char * argv[]) {
 				boost::none,
 				boost::none
 			};
-			//std::cerr << "seq.size(): " << ss.seq.size() << "; " << ss.seq << std::endl;
+			//evaluating the sequence
 			Utr3FinderFuzzy u3Fuzzy(ss);
 			std::vector<Utr3Finder::Utr3FinderResult> u3FuzzyResVector = u3Fuzzy.getPolyaMotifPos();
-			
-
-
+			//analyzing the results
 			for (auto resultIt = u3FuzzyResVector.begin(); resultIt != u3FuzzyResVector.end(); resultIt++) {
 				if (resultIt->pos == 100 && resultIt->strand == "+" && strand == "+") {
 					correctMatches++;
@@ -114,7 +119,7 @@ int main (int argc, char * argv[]) {
 						motif = temp;
 					}
 					
-					std::cerr << resultIt->truthValue << "  " << motif << std::endl;
+					//std::cerr << resultIt->truthValue << "  " << motif << std::endl;
 					if (Utr3FinderFuzzy::dseLocMap.find(motif) == Utr3FinderFuzzy::dseLocMap.end()) {
 						unknMotives++;			
 					}
@@ -123,9 +128,17 @@ int main (int argc, char * argv[]) {
 			analyzedSequences++;
 		}
 	}
-	std::cerr << "correct predictions: " << correctMatches << std::endl;
+	std::cerr << "-------Sensitivity test-------" << std::endl;
+	std::cerr << "correct predictions (found true positives): " << correctMatches << std::endl;
+	std::cerr << "incorrect predictions (false negatives): " << analyzedSequences - unknMotives - correctMatches << std::endl;
+	//std::cerr << "total sequences analyzed (total true positives): " << analyzedSequences << std::endl; 
 	std::cerr << "incorrect predictions: " << incorrectMatches << std::endl;
 	std::cerr << "unknown motives: " << unknMotives << std::endl;
-	std::cerr << "total sequences analyzed (true positives): " << analyzedSequences << std::endl; 
+	double correctness = static_cast<double>(correctMatches) / (analyzedSequences - unknMotives);
+	std::cerr << "sensitivity (w/o unknown motives): " << correctness << std::endl;
+	
+	
+	
+
 }
 
