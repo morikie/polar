85298c1a414bdbec4704fdc60f60e6d4dbec0abf
Moritz Kiekeben <moritz.k@fu-berlin.de>
2016-05-11 16:14:42 +0200

    fixed bug in folding script

--- scripts/len50_foldings.py
+++ scripts/len50_foldings.py
@@ -5,21 +5,25 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 
 
-iput = open("../bin/TPdataSet.fa", "r")
-oput = open("plus20_len50_foldings.faf", "a")
-seqLen = 50
-offset = 276
+iput = open("../perf_testing/positiveSet.fa", "r")
+oput = open("foldings_l200_o250.faf", "a")
+seqLen = 200
+offset = 250
 
 def writeOutput(ipt, opt, length, off):
 	faRecords = SeqIO.parse(ipt, "fasta")
 	for record in faRecords:
 		with tempfile.NamedTemporaryFile() as fp:
 			#print(">" + record.id)
+				
 			fp.write(">" + record.id + "\n")
 			#print(str(record.seq[off:off+length]))
-			fp.write(str(record.seq[off:off+length]))
+			if record.id[-1] == "-":
+				fp.write(str(record.seq[off+1:off+length+1].complement()))
+			else:
+				fp.write(str(record.seq[off:off+length]))
 			fp.flush()
-			cout = subprocess.check_output(["./RNAfold", "-i", fp.name])
+			cout = subprocess.check_output(["./RNAfold", "--noPS", "-i", fp.name])
 			opt.write(cout)
 
 if __name__ == "__main__":
